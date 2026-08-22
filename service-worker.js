9fe9517531cef44e360779d7ea0cb5d76c83309c
itohen0716 <mail.nori716@gmail.com>
2026-08-22 15:57:54 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 "use strict";
-const CACHE="kunkunshi-player-v4";
+const CACHE="kunkunshi-player-v5";
 const ASSETS=[
   "./","index.html","styles.css","player.css","player-layout-v3.css","sample-score.js","legacy-songs.js","player.js","manifest.webmanifest","icon.svg",
   "assets/fonts/ShipporiMincho-SemiBold-Kunkunshi.ttf","assets/fonts/ShipporiMincho-LICENSE.txt",
   "audio/合.wav","audio/乙.wav","audio/老.wav","audio/下老.wav","audio/四.wav","audio/上.wav",
   "audio/中.wav","audio/尺.wav","audio/工.wav","audio/五.wav","audio/六.wav","audio/七.wav","audio/八.wav",
   "audio/二揚四.wav","audio/二揚上.wav","audio/二揚_中.wav","audio/三下七.wav"
-  ,"kando-hd/cha1.png","kando-hd/tindami2.png","kando-hd/○.png","kando-hd/七.png","kando-hd/上.png","kando-hd/下尺.png","kando-hd/下老.png","kando-hd/中.png","kando-hd/乙.png","kando-hd/五.png","kando-hd/八.png","kando-hd/六.png","kando-hd/合.png","kando-hd/四.png","kando-hd/尺.png","kando-hd/工.png","kando-hd/老.png"
+  ,"kando-hd/cha1.gif","kando-hd/tindami2.png","kando-hd/○.png","kando-hd/七.png","kando-hd/上.png","kando-hd/下尺.png","kando-hd/下老.png","kando-hd/中.png","kando-hd/乙.png","kando-hd/五.png","kando-hd/八.png","kando-hd/六.png","kando-hd/合.png","kando-hd/四.png","kando-hd/尺.png","kando-hd/工.png","kando-hd/老.png"
 ];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
